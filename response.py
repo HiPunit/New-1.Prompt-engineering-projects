5f3062a435541d708b894f34fb692fbad28c49e1
HiPunit <146946440+HiPunit@users.noreply.github.com>
2023-11-17 17:26:55 +0530

    Create response.py
    part 1

---
+++ response.py
@@ -0,0 +1,8 @@
+import random
+
+def unknown():
+    response = ['Could you please re-phrase that?',
+                "...",
+                "Sounds about right",
+                "What does that mean?"][random.randrage(4)]
+    return response
